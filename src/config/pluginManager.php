a25e724ba4423e311a4bd2dcf7fadc972716c59b
baijunyao <baijunyao@baijunyao.com>
2018-05-21 14:49:49 +0800

    增加配置项示例

--- src/config/pluginManager.php
+++ src/config/pluginManager.php
@@ -1,7 +1,15 @@
 <?php
 
 return [
-    'manager' => [
-
+    'plugin' => [
+        /**
+         * 示例
+         */
+        // [
+        //     'name' => \Baijunyao\LaravelFlash\Manager::class,  // 要使用的插件
+        //     'except' => [
+        //         'home/*' // 排除的路由 此路由下不使用插件
+        //     ]
+        // ],
     ]
 ];
